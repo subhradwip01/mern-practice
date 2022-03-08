b0ac2d54bbf75da2a9a5cb229f10da841b638f8e
subhradwip01 <subhradwip01@gmail.com>
2022-03-08 10:49:59 +0530

    mongodb connected
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- mern-server/app.js
+++ mern-server/app.js
@@ -1,13 +1,31 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
 
+dotenv.config()
+
+const DB=process.env.DATABASE;
+console.log(DB)
 
 const app=express();
 
 // middlewire
 app.use(bodyParser.json())
+app.get("/",(req,res,next)=>{
+    res.send({
+        name:"Subhradwip",
+        age:20
+    })
+})
 
-// server
-app.listen(8080,()=>{
-    console.log("Server started at 8080 port")
+
+// server and database connection
+mongoose.connect(DB).then(()=>{
+    console.log("Connected to database")
+    app.listen(8080,()=>{
+        console.log("Server started at 8080 port")
+    })
+}).catch(err=>{
+    console.log("Error in connection")
 })
