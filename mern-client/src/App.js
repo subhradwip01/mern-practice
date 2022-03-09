bf004147d051f7dd19493366e24097d693f855c5
subhradwip01 <subhradwip01@gmail.com>
2022-03-09 11:38:45 +0530

    Intial component setup and Navbar done
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- mern-client/src/App.js
+++ mern-client/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Navbar , Home , Contact, About, Signup , Login, } from './components'
+import { Route,Switch } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    
+    <Navbar/>
+    <Switch>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route exact path="/contact">
+        <Contact/>
+      </Route>
+      <Route exact path="/about">
+        <About/>
+      </Route>
+      <Route exact path="/login">
+        <Login/>
+      </Route>
+      <Route exact path="/signup">
+        <Signup/>
+      </Route>
+    </Switch>
+    </>
   )
 }
 
---
+++ mern-client/src/components/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
+const Navbar = () => {
+  const navList = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+    {
+      name: "Contact",
+      path: "/contact",
+    },
+    {
+      name: "Login",
+      path: "/login",
+    },
+    {
+      name: "Signup",
+      path: "/signup",
+    },
+  ];
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Navbar
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto px-4">
+              {navList.map((nav,i) => (
+                <li key={i} className="nav-item">
+                  <Link className="nav-link" to={nav.path}>
+                    {nav.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
