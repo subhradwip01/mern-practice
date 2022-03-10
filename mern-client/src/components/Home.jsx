579b869721f36db3cf76d046adf9cca1864b639d
subhradwip01 <subhradwip01@gmail.com>
2022-03-10 16:02:58 +0530

    Home page created
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- mern-client/src/components/Home.jsx
+++ mern-client/src/components/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='d-flex align-items-center justify-content-center text-center flex-column home-bg' style={{"height":"100vh"}}>
+      <h1 style={{"fontWeight":"900","fontSize":"3rem"}}>Welcome to MERN Practice</h1>
+      <h3 style={{"color":"#0072ff"}}>Hello, Subhradwip 👋 </h3>
+    </div>
   )
 }
 
