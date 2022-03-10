8162e713f8327757d3e7c8c894aaf6628edc6cf2
subhradwip01 <subhradwip01@gmail.com>
2022-03-10 16:04:08 +0530

    About us page removed
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- mern-client/src/App.js
+++ mern-client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar , Home , Contact, About, Signup , Login, } from './components'
+import { Navbar , Home , Contact, About, Signup , Login,Error } from './components'
 import { Route,Switch } from 'react-router-dom'
 import "./App.css"
 
@@ -15,14 +15,14 @@ const App = () => {
       <Route exact path="/contact">
         <Contact/>
       </Route>
-      <Route exact path="/about">
-        <About/>
-      </Route>
       <Route exact path="/login">
         <Login/>
       </Route>
       <Route exact path="/signup">
         <Signup/>
+      </Route>
+      <Route path="*">
+        <Error/>
       </Route>
     </Switch>
     </>
